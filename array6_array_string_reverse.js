dde1545846308d6d33bc91cc4afdebfa279268fe
cbh808 <cbh808@gmail.com>
2022-01-25 11:08:01 +0100

    ok

--- array6_array_string_reverse.js
+++ array6_array_string_reverse.js
@@ -1,32 +1,31 @@
-// implement your own version of the Array.prototype.reverse method. 
-
-// implementation to differ from the built-in method as follows:
-//     It should accept either a string or an array as an argument
-//     It should return a new string or array
-
-/*
-input: collection, index-based
-output: collection, values at each index reversed
-
-Data structures:
-strings are primitives and chars cannot be reassigned directly
-array elements can be reassigned directly in a new result array
-
-Alg.
-check if string or array
-
-iterate over indexes from index 0 to end of collection
-
-
 function reverse(inputForReversal) {
-  
+  if (typeof inputForReversal === 'string') {
+    return reverseString(inputForReversal);
+  } else {
+    return reverseArray(inputForReversal);
+  }
 }
 
-reverse('Hello');           // "olleH"
-reverse('a');               // "a"
-reverse([1, 2, 3, 4]);      // [4, 3, 2, 1]
-reverse([]);                // []
+function reverseString(string) {
+  let stringAsArray = string.split('')
+  let array = reverseArray(stringAsArray);
+  return array.join('')
+}
+
+function reverseArray(array) {
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    result[result.length] = array[i];
+  }
+  return result;
+}
+
+
+console.log(reverse('Hello'));           // "olleH"
+console.log(reverse('a'));               // "a"
+console.log(reverse([1, 2, 3, 4]));      // [4, 3, 2, 1]
+console.log(reverse([]));                // []
 
 const array = [1, 2, 3];
-reverse(array);             // [3, 2, 1]
-array;                      // [1, 2, 3]
+console.log(reverse(array));             // [3, 2, 1]
+console.log(array);                      // [1, 2, 3]
