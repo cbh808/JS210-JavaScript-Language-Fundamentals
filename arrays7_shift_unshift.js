0661dcc9ac94e94aaadcb6147cbf2e022f6582c9
cbh808 <cbh808@gmail.com>
2022-01-27 01:02:22 +0100

    description

---
+++ arrays7_shift_unshift.js
@@ -0,0 +1,40 @@
+/*
+Array Shift and Unshift
+implement own Array.prototype.shift and Array.prototype.unshift methods.
+
+shift removes the first element from array
+returns that element
+if array empty, shift returns undefined.
+
+unshift adds one or more elements to the start of an array
+returns new length of the array
+
+Both methods mutate the original array.
+*/
+function shift(array) {
+  let firstElement = array[0];  // undefined if array is empty
+  array.splice(0, 1);
+  return firstElement;
+}
+
+function unshift(array, ...args) {
+  for (let i = 0; i < args.length; i += 1) {
+    array.splice(0, 0, args[i]);
+  }
+
+  return array.length;
+}
+
+console.log(shift([1, 2, 3]));                // 1
+console.log(shift([]));                       // undefined
+console.log(shift([[1, 2, 3], 4, 5]));        // [1, 2, 3]
+
+console.log(unshift([1, 2, 3], 5, 6));        // 5
+console.log(unshift([1, 2, 3]));              // 3
+console.log(unshift([4, 5], [1, 2, 3]));      // 3
+
+const testArray = [1, 2, 3];
+console.log(shift(testArray));                // 1
+console.log(testArray);                       // [2, 3]
+console.log(unshift(testArray, 5));           // 3
+console.log(testArray);                       // [5, 2, 3]
